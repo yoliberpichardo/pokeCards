5bdbf810f84257d1810201bd175c541c00cd03ef
Yoliber Pichardo <yoliberfelixpp01@gmail.com>
2022-05-31 22:34:45 -0400

    added rotation to cards

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -1,33 +1,35 @@
 <template>
     <div v-if="pokeData"  class="bodyCard">
-        <div v-for="poke in pokeData"  :key="poke.id" class="subBodyCard">
-            <div class="up-menu">
-                <div class="pokeID">
-                    <h1> {{poke.id}}</h1>
-                </div>
-                <div class="infoContent">
-                    <div class="iconsFav" :id="poke.id" @click="changeIcons(poke.id)">
-                            <img :src="start[poke.id]" :alt="poke.id">
+        <div v-for="poke in pokeData"  :key="poke.id" class="subBodyCard" :style="rotation">
+            <div class="rotationBody" :style="rotation">
+                <div class="up-menu">
+                    <div class="pokeID">
+                        <h1> {{poke.id}}</h1>
                     </div>
-                    <div class="iconsFav">
-                        <img :src="pokeInfo" :alt="poke.id">
+                    <div class="infoContent">
+                        <div class="iconsFav" @click="changeIcons(poke.id)">
+                                <img :src="start[poke.id]"  :alt="poke.id">
+                        </div>
+                        <div class="iconsFav" :id="poke.id" @click="rotationBody($event,poke.id)">
+                            <img :src="info[poke.id]" :alt="poke.id">
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="imgContent">
-                <img :src="poke.sprites.other.home.front_default" :alt="poke.forms.name" >
-            </div>
-            <div class="descriptionContent">
-                <div class="nameContent">
-                    <h1>{{poke.name}}</h1>
+                <div class="imgContent">
+                    <img :src="poke.sprites.other.home.front_default" :alt="poke.forms.name" >
                 </div>
-                <div class="typesContent">
-                    <h3 v-if="poke.types[0].type.name">
-                        {{poke.types[0].type.name}}
-                    </h3>
-                    <h3 v-if="poke.types[1]">
-                        {{poke.types[1].type.name}}
-                    </h3>
+                <div class="descriptionContent">
+                    <div class="nameContent">
+                        <h1>{{poke.name}}</h1>
+                    </div>
+                    <div class="typesContent">
+                        <h3 v-if="poke.types[0].type.name">
+                            {{poke.types[0].type.name}}
+                        </h3>
+                        <h3 v-if="poke.types[1]">
+                            {{poke.types[1].type.name}}
+                        </h3>
+                    </div>
                 </div>
             </div>
         </div>
@@ -36,16 +38,18 @@
 
 <script>
 import start1 from '../assets/starregular.svg'
-import start2 from '../assets/starsolid.svg'
-import pokeInfo from '../assets/infosolid.svg'
+import start2 from '../assets/startyellow.svg'
+import pokeInfo1 from '../assets/infoBlack.svg'
+import pokeInfo2 from '../assets/infoWhite.svg'
 export default {
     name: 'Card',
     props:['pokeData'],
     data(){
         return{
             data: false,
-            pokeInfo: pokeInfo,
             start:[''],
+            info: [''],
+            rotation: '',
             colorType:{
                 normal:{
                     color:'#898585'
@@ -85,8 +89,14 @@ export default {
             if(this.pokeData.length > 0){
                 this.pokeData.forEach((poke)=> {
                     this.start.push(start1)
+                    this.info.push(pokeInfo1)
                 })
             }
+        },
+        rotationBody(e,id){
+            console.log(e.path[1].id);
+            this.info[id] === pokeInfo1 ? this.info[id] = pokeInfo2 : this.info[id] = pokeInfo1
+            this.rotation == '' ? this.rotation = 'transform: rotateY(180deg)' : this.rotation = ''
         }
     },
     watch:{
@@ -111,7 +121,22 @@ export default {
         background-color: #a9d9ea;
         border-radius: 20px;
         box-shadow: 0 0 10px #272727d8;
+        perspective: 1000px;
     }
+
+    .rotationBody{
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: transform 0.6s;
+        transform-style: preserve-3d;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    }
+
+    /* .subBodyCard:hover .rotationBody{
+        transform: rotateY(180deg);
+    } */
 
     .imgContent img {
         max-width: 30 0px;
